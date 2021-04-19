06af62ba7027ecf418234a3619104a8ae40be0fa
kamilnr10 <kamil.nr10@gmail.com>
2021-04-19 20:40:35 +0200

    feat: add axios, add content from datoCMS

--- src/components/templates/NewsSection/NewsSection.js
+++ src/components/templates/NewsSection/NewsSection.js
@@ -1,44 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Wrapper, NewsSectionHeader, ArticleWrapper, TitleWrapper, ContentWrapper } from './NewsSection.styles';
-import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper';
 import { Button } from 'components/atoms/Button/Button';
+import axios from 'axios';
 
-const data = [
-  {
-    title: 'New computers at school',
-    category: 'Tech news',
-    content:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus provident quos perspiciatis aperiam odit tempora illo, adipisci, maxime exercitationem architecto, iusto expedita animi. Eligendi, perspiciatis! Unde tempore saepe placeat veritatis.',
-    image: 'https://unsplash.it/500/400',
-  },
-  {
-    title: '2New computers at school',
-    category: 'Tech news',
-    content:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus provident quos perspiciatis aperiam odit tempora illo, adipisci, maxime exercitationem architecto, iusto expedita animi. Eligendi, perspiciatis! Unde tempore saepe placeat veritatis.',
-    image: 'https://unsplash.it/500/400',
-  },
-  {
-    title: '3New computers at school',
-    category: 'Tech news',
-    content:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus provident quos perspiciatis aperiam odit tempora illo, adipisci, maxime exercitationem architecto, iusto expedita animi. Eligendi, perspiciatis! Unde tempore saepe placeat veritatis.',
-    image: 'https://unsplash.it/500/400',
-  },
-  {
-    title: '3New computers at school',
-    category: 'Tech news',
-    content:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus provident quos perspiciatis aperiam odit tempora illo, adipisci, maxime exercitationem architecto, iusto expedita animi. Eligendi, perspiciatis! Unde tempore saepe placeat veritatis.',
-    image: 'https://unsplash.it/500/400',
-  },
-];
+const API_TOKEN = '7cd8cf80a54919f0e2dbf2480379b0';
+
+// const data = [
+//   {
+//     title: 'New computers at school',
+//     category: 'Tech news',
+//     content:
+//       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus provident quos perspiciatis aperiam odit tempora illo, adipisci, maxime exercitationem architecto, iusto expedita animi. Eligendi, perspiciatis! Unde tempore saepe placeat veritatis.',
+//     image: 'https://unsplash.it/500/400',
+//   },
+//   {
+//     title: '2New computers at school',
+//     category: 'Tech news',
+//     content:
+//       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus provident quos perspiciatis aperiam odit tempora illo, adipisci, maxime exercitationem architecto, iusto expedita animi. Eligendi, perspiciatis! Unde tempore saepe placeat veritatis.',
+//     image: 'https://unsplash.it/500/400',
+//   },
+//   {
+//     title: '3New computers at school',
+//     category: 'Tech news',
+//     content:
+//       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus provident quos perspiciatis aperiam odit tempora illo, adipisci, maxime exercitationem architecto, iusto expedita animi. Eligendi, perspiciatis! Unde tempore saepe placeat veritatis.',
+//     image: 'https://unsplash.it/500/400',
+//   },
+//   {
+//     title: '3New computers at school',
+//     category: 'Tech news',
+//     content:
+//       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus provident quos perspiciatis aperiam odit tempora illo, adipisci, maxime exercitationem architecto, iusto expedita animi. Eligendi, perspiciatis! Unde tempore saepe placeat veritatis.',
+//     image: 'https://unsplash.it/500/400',
+//   },
+// ];
 
 const NewsSection = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    axios
+      .post(
+        'https://graphql.datocms.com/',
+        {
+          query: `
+            { 
+              allArticles {
+                id
+                title
+                category
+                content
+                image {
+                  id
+                }
+              }
+              }`,
+        },
+        {
+          headers: {
+            authorization: `Bearer ${API_TOKEN}`,
+          },
+        }
+      )
+      .then(({ data: { data } }) => {
+        console.log(data);
+        setArticles(data.allArticles);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Wrapper>
       <NewsSectionHeader>News feed section</NewsSectionHeader>
-      {data.map(({ title, category, content, image = null }) => (
+      {articles.map(({ title, category, content, image = null }) => (
         <ArticleWrapper key={title}>
           <TitleWrapper>
             <h3>{title}</h3>
@@ -46,7 +81,7 @@ const NewsSection = () => {
           </TitleWrapper>
           <ContentWrapper>
             <p>{content}</p>
-            {image ? <img src={image} alt="article" /> : null}
+            {image ? <img src={image.url} alt="article" /> : null}
           </ContentWrapper>
           <Button>Read me</Button>
         </ArticleWrapper>
