12995632808a6e4755a4a901a37f6ae5b3355a94
kamilnr10 <kamil.nr10@gmail.com>
2021-04-19 20:46:10 +0200

    fix: repair axios query data

--- src/components/templates/NewsSection/NewsSection.js
+++ src/components/templates/NewsSection/NewsSection.js
@@ -52,7 +52,7 @@ const NewsSection = () => {
                 category
                 content
                 image {
-                  id
+                  url
                 }
               }
               }`,
